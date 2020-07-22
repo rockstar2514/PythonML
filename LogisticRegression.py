b3b6ef80780fd15fac9f9e7506be25b8ea97e8d3
rockstar2514 <56267835+rockstar2514@users.noreply.github.com>
2020-07-22 13:06:50 +0530

    Update LogisticRegression.py

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -10,7 +10,7 @@ def LinearRegression(X,y,theta,itera,alpha):
         predicted=1/(1+np.exp(X@theta))
         grad=(1/r)*(np.transpose(X)@(predicted-y))
         theta=theta-alpha*grad
-        error=(-1/r)*(np.transpose(y)@np.log(predicted)+np.transapose(1-y)@(np.log(1-predicted)))
+        error=(-1/r)*(np.transpose(y)@np.log(predicted)+np.transpose(1-y)@(np.log(1-predicted)))
         z.append(error[0][0])     
     predicted=1/(1+np.exp(theta))
     plt.plot(x,z)
